6c6c5b355bf4a7417ca43190374894916606bbb5
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2018-11-11 15:21:23 -0500

    Beautiful Conveyor

--- src/game/Conveyor.java
+++ src/game/Conveyor.java
@@ -4,9 +4,10 @@ import java.awt.*;
 import java.util.ArrayList;
 
 import com.zalinius.physics.Point2D;
+import com.zalinius.utilities.time.GameClock;
 
 public class Conveyor extends Edge {
-	
+	private static final int SECTION_WIDTH = 10;
 	public Conveyor(Point2D start, Point2D end, Node nextNode) {
 		super(start, end, nextNode);
 	}
@@ -15,6 +16,15 @@ public class Conveyor extends Edge {
 	public void render(Graphics2D g) {	
 		super.render(g);
 		g.setColor(Color.WHITE);
-		g.drawLine((int)start.x - Node.HALF_NODE_SIZE, (int)(start.y + Node.HALF_NODE_SIZE), (int)end.x + Node.HALF_NODE_SIZE, (int)(end.y + Node.HALF_NODE_SIZE));
+		for(int i = (int) start.x - Node.HALF_NODE_SIZE; i < end.x + Node.HALF_NODE_SIZE ; ++i) {
+			if(((i - (int)(GameClock.timeNow() * edgeSpeed)) / SECTION_WIDTH) % 2 == 0) {
+				g.setColor(Color.GRAY);
+			}
+			else {
+				g.setColor(Color.WHITE);
+			}
+			g.drawLine(i, (int)(start.y + Node.HALF_NODE_SIZE), i+1, (int)(start.y + Node.HALF_NODE_SIZE));
+		}
+		//g.drawLine((int)start.x - Node.HALF_NODE_SIZE, (int)(start.y + Node.HALF_NODE_SIZE), (int)end.x + Node.HALF_NODE_SIZE, (int)(end.y + Node.HALF_NODE_SIZE));
 	}
 }
